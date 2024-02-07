6d2ff5ea8bb092f195ba3356836438c823418c4e
1190190 <fayaaz.nabie@hva.nl>
2024-02-07 10:33:58 +0100

    eerste push

--- script/scripts.js
+++ script/scripts.js
@@ -1,12 +1,12 @@
 
-fetch('https://fayaaz036.github.io/WAPS/data.json')
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        return response;
-    })
-    .then((response) => {
-        console.log(response.name);
-    });
+// fetch('https://fayaaz036.github.io/WAPS/data.json')
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+//
+//         return response;
+//     })
+//     .then((response) => {
+//         console.log(response);
+//     });
